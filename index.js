3f0a58b067ad0a6068f1d46663c9fc341b5f7555
blevinspercussion <blevinspercussion@gmail.com>
2022-07-18 11:51:56 -0500

    Added images and refined css styling for sub-menus

--- index.js
+++ index.js
@@ -20,19 +20,33 @@ companyMenuDiv.classList.add('company');
 
 
 // Create items for featuresMenu
-const todoList = document.createElement("li");
 const todoListIcon = document.createElement("img");
-const calendar = document.createElement("li");
+const todoList = document.createElement("li");
 const calendarIcon = document.createElement("img");
-const reminders = document.createElement("li");
+const calendar = document.createElement("li");
 const remindersIcon = document.createElement("img");
-const planning = document.createElement("li");
+const reminders = document.createElement("li");
 const planningIcon = document.createElement("img");
+const planning = document.createElement("li");
+
+
+// Add src and class to images 
+todoListIcon.src = "img/icon-todo.svg";
+calendarIcon.src = "img/icon-calendar.svg";
+remindersIcon.src = "img/icon-reminders.svg";
+planningIcon.src = "img/icon-planning.svg";
+
+todoListIcon.classList.add('menu-icon');
+calendarIcon.classList.add('menu-icon');
+remindersIcon.classList.add('menu-icon');
+planningIcon.classList.add('menu-icon');
+
 
 // Create items for companyMenu
 const history = document.createElement("li");
 const ourTeam = document.createElement("li");
 const blog = document.createElement("li");
+
 
 // Add text to list items
 todoList.textContent = "Todo List";
@@ -44,24 +58,27 @@ history.textContent = "History";
 ourTeam.textContent = "Our Team";
 blog.textContent = "Blog";
 
+// Add images
+todoList.appendChild(todoListIcon);
+calendar.appendChild(calendarIcon);
+reminders.appendChild(remindersIcon);
+planning.appendChild(planningIcon);
 
 // Construct menus
+
 featuresMenuDiv.appendChild(todoList);
 featuresMenuDiv.appendChild(calendar);
 featuresMenuDiv.appendChild(reminders);
 featuresMenuDiv.appendChild(planning);
 
+
 companyMenuDiv.appendChild(history);
 companyMenuDiv.appendChild(ourTeam);
 companyMenuDiv.appendChild(blog);
 
-
-
-
-
-
 // Add event listeners 
 features.addEventListener('mouseenter', () => {
+    console.log(todoListIcon.src);
     features.appendChild(featuresMenuDiv);
 });
 
