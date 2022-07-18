a06938448c5dbf8e5fa439d96578030954207bc6
blevinspercussion <blevinspercussion@gmail.com>
2022-07-18 10:53:34 -0500

    Start index.js. Set up DOM constants and dynamic creation for most menu items

---
+++ index.js
@@ -0,0 +1,43 @@
+// Get DOM elements 
+const features = document.getElementById("features");
+const company = document.getElementById("company");
+const careers = document.getElementById("careers");
+const about = document.getElementById("about");
+
+const login = document.getElementById("login");
+const register = document.getElementById("register");
+
+// Create submenus for navbar
+const featuresMenuDiv = document.createElement("div");
+const companyMenuDiv = document.createElement("div");
+
+const featuresMenu = document.createElement("ul");
+const companyMenu = document.createElement("ul");
+
+// Create items for featuresMenu
+const todoList = document.createElement("li");
+const todoListIcon = document.createElement("img");
+const calendar = document.createElement("li");
+const calendarIcon = document.createElement("img");
+const reminders = document.createElement("li");
+const remindersIcon = document.createElement("img");
+const planning = document.createElement("li");
+const planningIcon = document.createElement("img");
+
+
+// Create items for companyMenu
+const history = document.createElement("li");
+const outTeam = document.createElement("li");
+const blog = document.createElement("li");
+
+// Create 
+
+// Construct menus
+
+
+
+// Add event listeners 
+
+
+
+// Append items
